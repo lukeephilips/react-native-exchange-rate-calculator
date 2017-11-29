6597f41692958a3dd8c181469e13fe2ff7ea94ff
Luke <luke.e.philips@gmail.com>
2017-11-29 15:31:12 -0800

    animated resizing of logo

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,23 +1,97 @@
-import React from 'react';
-import { View, Image, ImageBackground, Text} from 'react-native';
+import React, {Component} from 'react';
+import { View, Image, ImageBackground, Text, Keyboard, Animated, Platform, StyleSheet } from 'react-native';
 import styles from './styles'
 
-const Logo = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      resizeMode="contain"
-      style={styles.containerImage}
-      source={require('./images/background.png')}>
-      <Image
-        resizeMode="contain"
-        style={styles.image}
-        source={require('./images/logo.png')}
-      />
-    </ImageBackground>
-    <Text
-      style={styles.text} >Currency Converter
-    </Text>
-  </View>
-);
+const ANIMATION_DURATION = 250;
+
+class Logo extends React.Component {
+  constructor(props){
+    super(props);
+    this.containerImageWidth = new Animated.Value(styles.$largeContainerSize);
+    this.imageWidth = new Animated.Value(styles.$largeImageSize);
+
+  }
+  componentDidMount() {
+    let showListener = `keyboardDidShow`
+    let hideListener = `keyboardDidHide`
+    if (Platform.OS === 'ios') {
+      console.log("ios listener");
+      showListener = `keyboardWillShow`
+      let hideListener = `keyboardDidHide`
+    }
+
+    this.keyboardDidShowListener = Keyboard.addListener(
+      showListener,
+      this.keyboardShow,
+    );
+    this.keyboardDidHideListener = Keyboard.addListener(
+      hideListener,
+      this.keyboardHide,
+    );
+  }
+  componentWillUnmount() {
+    this.keyboardShowListener.remove();
+    this.keyboardHideListener.remove();
+  }
+
+  keyboardShow = () => {
+    Animated.parallel([
+      Animated.timing(this.containerImageWidth, {
+        toValue: styles.$smallContainerSize,
+        duration: ANIMATION_DURATION,
+      }),
+      Animated.timing(this.imageWidth, {
+        toValue: styles.$smallImageSize,
+        duration: ANIMATION_DURATION,
+      })
+    ]).start();
+  };
+
+  keyboardHide = () => {
+    Animated.parallel([
+      Animated.timing(this.containerImageWidth, {
+        toValue: styles.$largeContainerSize,
+        duration: ANIMATION_DURATION,
+      }),
+      Animated.timing(this.imageWidth, {
+        toValue: styles.$largeImageSize,
+        duration: ANIMATION_DURATION,
+      })
+    ]).start();
+  }
+
+  render() {
+    const containerImageStyle = [
+      styles.container,
+      {width: this.containerImageWidth,
+      height: this.containerImageWidth}
+    ];
+    const imageStyle = [
+      styles.image,
+      {width: this.imageWidth,
+      height: this.imageWidth},
+    ];
+
+    return (
+      <View style={styles.container}>
+        <Animated.View style={containerImageStyle}>
+          <Animated.Image
+            resizeMode="contain"
+            style={[StyleSheet.absoluteFill, containerImageStyle]}
+            source={require('./images/background.png')}
+          />
+          <Animated.Image
+            resizeMode="contain"
+            style={imageStyle}
+            source={require('./images/logo.png')}
+          />
+        </Animated.View>
+        <Text
+          style={styles.text} >Currency Converter
+        </Text>
+      </View>
+    )
+  }
+};
 
 export default Logo;
--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,23 +4,30 @@ import {Dimensions} from 'react-native';
 const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
+
   container: {
-    alignItems: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   containerImage: {
-    alignItems: "center",
-    justifyContent: "center",
-    width: imageWidth,
-    height: imageWidth,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '$largeContainerSize',
+    height: '$largeContainerSize',
   },
   image: {
-    width: imageWidth / 2,
+    width: '$largeImageSize',
+    tintColor: '$primaryBlue',
   },
   text: {
-    fontWeight: "600",
+    fontWeight: '600',
     fontSize: 28,
     letterSpacing: -0.5,
     marginTop: 10,
-    color: "$white",
+    color: '$white',
   }
 });
