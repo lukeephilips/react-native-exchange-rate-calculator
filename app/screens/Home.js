e74998ad4dd211fd11d52daa72ff6dab2cf779d7
Luke <luke.e.philips@gmail.com>
2017-11-29 12:12:08 -0800

    add settings button

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,12 +5,19 @@ import {Container} from './../components/Container';
 import {Logo} from './../components/Logo';
 import {InputWithButton} from './../components/TextInput';
 import {ClearButton} from './../components/Button';
+import {LastConverted} from './../components/Text';
+import {Header} from './../components/Header';
+
+
 
 
 const TEMP_BASE_CURRENCY = "USD";
 const TEMP_QUOTE_CURRENCY = "GBP";
 const TEMP_BASE_PRICE = "100";
 const TEMP_QUOTE_PRICE = "81.02";
+const TEMP_CONVERSION_RATE = .8102;
+const TEMP_CONVERSION_DATE = new Date();
+
 
 class Home extends React.Component {
   handlePressBaseCurrency = () => {
@@ -25,11 +32,17 @@ class Home extends React.Component {
   handleSwapCurrency = (text) => {
     console.log('press swap currency');
   }
+  handleSettingsPress = (text) => {
+    console.log('press settings');
+  }
 
   render() {
     return(
       <Container>
         <StatusBar backgroundColor="red" translucent={true} barStyle="default"/>
+        <Header
+          onPress={this.handleSettingsPress}
+        />
         <Logo />
         <InputWithButton
           buttonText={TEMP_BASE_CURRENCY}
@@ -43,6 +56,12 @@ class Home extends React.Component {
           onPress={this.handlePressQuoteCurrency}
           editable={false}
           defaultValue={TEMP_QUOTE_PRICE}
+         />
+         <LastConverted
+           base={TEMP_BASE_CURRENCY}
+           quote={TEMP_QUOTE_CURRENCY}
+           date={TEMP_CONVERSION_DATE}
+           conversionRate={TEMP_CONVERSION_RATE}
          />
          <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
       </Container>
